8476881d6a9b229268c0d6901256a240db28f2bc
Flavien Bonvin <bonin.flavien@gmail.com>
2018-09-24 18:30:30 +0200

    Removing all files in .gitignore

--- kotlin/src/main/kotlin/game/Tank.kt
+++ kotlin/src/main/kotlin/game/Tank.kt
@@ -1,12 +1,3 @@
 package main.kotlin.game
 
-data class Tank(val name: String, val country: String, var hp: Double, var isAlive: Boolean) {
-
-    override fun toString(): String {
-        return """
-            Tank information:
-            Name: $name, Country: $country, HP: $hp, Is alive: $isAlive
-
-        """.trimIndent()
-    }
-}
+data class Tank(val name: String, val country: String, var hp: Double, var isAlive: Boolean) {}
